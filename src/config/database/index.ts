4e53c5c0e119cb331be4e9c2a81a444df141d346
Sulton <msulton55@gmail.com>
2021-01-09 15:22:44 +0700

    adding useFindAndModify property to Mongoose.connect

--- src/config/database/index.ts
+++ src/config/database/index.ts
@@ -2,10 +2,15 @@ import Mongoose from "mongoose";
 import Logger from "@utils/logger";
 
 // Database Connect
-Mongoose.connect("mongodb://localhost:27017/marala-klinik", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+Mongoose.connect(
+    // eslint-disable-next-line max-len
+    "mongodb+srv://marala-master:4-_1tiAGVCi-Zv@cluster0.xzlau.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    }
+)
     .then(() => Logger.info("database connect"))
     .catch((err) => {
         Logger.err(err);
