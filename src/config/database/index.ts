365606980c20c3758f861bc9c42f3c1928cc9a63
Sulton <msulton55@gmail.com>
2021-01-10 23:11:47 +0700

    config new mongoose connect

--- src/config/database/index.ts
+++ src/config/database/index.ts
@@ -4,12 +4,7 @@ import Logger from "@utils/logger";
 // Database Connect
 Mongoose.connect(
     // eslint-disable-next-line max-len
-    "mongodb+srv://marala-master:4-_1tiAGVCi-Zv@cluster0.xzlau.mongodb.net/marala-clinic?retryWrites=true&w=majority",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    }
+    "mongodb+srv://marala-master:4-_1tiAGVCi-Zv@cluster0.xzlau.mongodb.net/marala-clinic?retryWrites=true&w=majority", {useNewUrlParser: true,}
 )
     .then(() => Logger.info("database connect"))
     .catch((err) => {
