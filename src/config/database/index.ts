2cd980330ecc0225e35e1f2938ab9418023b3af7
Sulton <msulton55@gmail.com>
2021-01-10 23:26:38 +0700

    fix mongoose configuration

--- src/config/database/index.ts
+++ src/config/database/index.ts
@@ -1,11 +1,15 @@
 import Mongoose from "mongoose";
 import Logger from "@utils/logger";
 
+const uri = "mongodb+srv://marala-master:4-_1tiAGVCi-Zv@cluster0.xzlau.mongodb.net/marala-clinic?retryWrites=true&w=majority";
+
 // Database Connect
-Mongoose.connect(
-    // eslint-disable-next-line max-len
-    "mongodb+srv://marala-master:4-_1tiAGVCi-Zv@cluster0.xzlau.mongodb.net/marala-clinic?retryWrites=true&w=majority", {useNewUrlParser: true,}
-)
+Mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
     .then(() => Logger.info("database connect"))
     .catch((err) => {
         Logger.err(err);
